5238c79f8e417f9f2cec6a15b44857c65b691d91
Krystian Maziarz <maziarz.krystian@gmail.com>
2025-05-10 13:13:18 +0200

    creating services

---
+++ recipe-service/src/test/java/pl/com/recipeservice/RecipeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.recipeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
